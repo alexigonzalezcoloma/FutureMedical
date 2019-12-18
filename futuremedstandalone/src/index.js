a17e5560c82e3f392aa929d87f34f6d61c805173
Alexi Gonzalez Coloma <agonzalez2016@alu.uct.cl>
2019-12-18 13:23:06 -0300

    subida final
    70

---
+++ futuremedstandalone/src/index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+
+const url = require('url');
+const path = require('path');
+
+let mainWindow;
+
+if(process.env.NODE_ENV !== 'production') {
+    require('electron-reload')(__dirname, {
+      electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+    });
+  }
+
+app.on('ready', () => {
+
+    // The Main Window
+    mainWindow = new BrowserWindow({width: 720, height: 600, title: 'FutureMedical'});
+  
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'views/index.html'),
+      protocol: 'file',
+      slashes: true
+    }))
+
+});
